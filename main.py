5ea57f00410f34612a124e52720d0e838501dd9f
housong12590 <304536797@qq.com>
2019-02-27 13:33:48 +0800

    asdfasdf

--- main.py
+++ main.py
@@ -69,10 +69,11 @@ def total_count():
     ret = SQLHelper.fetch_one('SELECT count(*) AS count FROM users')
     total_count = ret.get('count')
     users = SQLHelper.fetch_all(
-        'SELECT id user_id,nickname,avatar,lng `long`,lat,DATE_FORMAT(created_at,"%Y-%m-%d %H:%m:%s") login_time FROM users WHERE to_days(created_at) = to_days(now()) AND `lng` IS NOT NULL ORDER BY id DESC LIMIT 50')
+        "SELECT id user_id,nickname,avatar,lng `long`,lat,DATE_FORMAT(created_at,'%%Y-%%m-%%d %%H:%%m:%%s') login_time FROM users WHERE to_days(created_at) = to_days(now()) AND `lng` IS NOT NULL ORDER BY id DESC LIMIT 50")
     today_count = len(users)
     ret = SQLHelper.fetch_one(
-        "SELECT COUNT(*) FROM users WHERE DATE_FORMAT( created_at,'%Y-%m-%d') = DATE_FORMAT(CURDATE()-1,'%Y-%m-%d');")
+        "SELECT COUNT(*) count FROM users WHERE DATE_FORMAT( created_at,'%%Y-%%m-%%d') = DATE_FORMAT(CURDATE()-1,'%%Y-%%m-%%d');")
+    print(ret)
     yesterday_count = ret.get('count')
     data = {'today_count': today_count, 'total_count': total_count,
             'yesterday_count': yesterday_count, 'users': users}
@@ -82,7 +83,7 @@ def total_count():
 @app.route('/user')
 def user():
     interval = request.args.get('interval', 3)
-    sql = 'SELECT id user_id,nickname,avatar,lng as `long`,lat,DATE_FORMAT(created_at,"%Y-%m-%d %H:%m:%s") login_time FROM users WHERE created_at > DATE_sub(NOW(), INTERVAL %s SECOND) and lng is not null' % (
+    sql = "SELECT id user_id,nickname,avatar,lng AS `long`,lat,DATE_FORMAT(created_at,'%%Y-%%m-%%d %%H:%%m:%%s') login_time FROM users WHERE created_at > DATE_sub(NOW(), INTERVAL {} SECOND) AND lng IS NOT NULL".format(
         interval)
     ret_data = SQLHelper.fetch_all(sql)
     return make_resp(ret_data)
